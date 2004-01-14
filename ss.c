380f87d30ba539befb1c1620f03527266da0df24
Fish <>
2004-01-14 11:41:12 +0000

    and once mroe for the dummies

--- ss.c
+++ ss.c
@@ -1,5 +1,5 @@
-/* NeoStats - IRC Statistical Services Copyright (c) 1999-2002 NeoStats Group Inc.
-** Copyright (c) 1999-2002 Adam Rutter, Justin Hammond
+/* NeoStats - IRC Statistical Services Copyright (c) 1999-2004 NeoStats Group Inc.
+** Copyright (c) 1999-2004 Adam Rutter, Justin Hammond
 ** http://www.neostats.net/
 **
 **  Portions Copyright (c) 2000-2001 ^Enigma^
